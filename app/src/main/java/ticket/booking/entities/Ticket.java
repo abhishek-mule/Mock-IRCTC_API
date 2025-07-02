33b3063b1bb4d088a042d2eaa3a138e432ea739d
Abhishek Mule <abhimule2709@gmail.com>
2025-07-03 00:00:22 +0530

    Initial commit

---
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -0,0 +1,15 @@
+package ticket.booking.entities;
+
+import java.util.Date;
+
+public class Ticket {
+    private String sourse;
+    private String destiantion;
+    private String userId;
+    private String ticketId;
+    private Date dateOfTravel;
+    private Train train;
+
+
+}
+
---
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,15 @@
+package ticket.booking.entities;
+
+public class User {
+
+
+    private String name;
+
+    private String password;
+
+    private String hashedpasswords;
+
+    private List<Tickets> ticketsBooked;
+
+    private String userId;
+}
