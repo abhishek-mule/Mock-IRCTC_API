f9e8682b1eac97514ec5ce8098a12438c1147fc3
Abhishek Mule <abhimule2709@gmail.com>
2025-07-10 00:07:16 +0530

    Updated UserBookingService.java

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class UserBookingService {
@@ -13,15 +14,29 @@ public class UserBookingService {
 
     private List<User> userList;
 
+    private ObjectMapper objectMapper   = new ObjectMapper();
+
     private static final ObjectMapper = new ObjectMapper();
 
-    private static final String USERS_PATH = "../localDB/users.json";
+    private static final String USERS_PATH = "app/src/main/java/ticket/booking/localDB/users.json";
 
-    private UserBookingService(User user1) {
+    private UserBookingService(User user1) throws IOException {
         this.user = user1;
          File users = new File(USERS_PATH);
          userList = ObjectMapper.readValue(users, new TypeReference<List<User>>() {
          })
+
+    public boolean signUp(User user1){
+            try{
+                userList.add(user1);
+                saveUserListToFile();
+                return Boolean.TRUE;
+
+            }catch (IDException ex){
+                return Boolean.FALSE;
+            }
+
+        }
 
     }
 
